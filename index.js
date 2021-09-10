695b0f1b6468772d912d9daaf58b8a608ba9951f
shreder0804 <shreder0804@gmail.com>
2021-09-10 20:53:18 +0900

    DOM 공부

---
+++ index.js
@@ -0,0 +1,33 @@
+import axios from './node_modules/axios/dist/axios.min.js';
+
+axios
+  .get('https://0341113f-6211-4c3a-b909-a5707803615d.mock.pstmn.io/product')
+  .then((res) => {
+    console.log(res.data);
+    const products = res.data.products;
+
+    let productsHtml = '';
+
+    for (let i = 0; i < products.length; i++) {
+      let product = products[i];
+      productsHtml += `
+    <div class="product-card">
+      <div>
+        <img class="product-img" src=${product.imageUrl} />
+        <div class="product-contents">
+          <span class="product-name">${product.name}</span>
+          <span class="product-price">${product.price}원</span>
+          <div class="product-seller">
+            <img class="product-avatar" src='images/icons/avatar.png' />
+            <span>${product.seller}</span>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    }
+
+    document.querySelector('#product-list').innerHTML = productsHtml;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
